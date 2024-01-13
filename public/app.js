d369a65d1c8f31beebb133367132ac3d5bc965df
Corentin Rouff <62677123+Legion-in-hell@users.noreply.github.com>
2024-01-13 04:24:30 +0100

    Fix formatting issues in index.html and server.js

--- public/app.js
+++ public/app.js
@@ -1,64 +1,181 @@
-document.querySelectorAll('.game-btn').forEach(btn => {
-    btn.addEventListener('click', function() {
-        let command;
-        if (this.classList.contains('attack')) {
-            command = '!attack';
-        } else if (this.classList.contains('flee')) {
-            command = '!flee';
-        } else if (this.classList.contains('rest')) {
-            command = '!rest';
-        } else if (this.classList.contains('wakeup')) {
-            command = '!wakeup';
-        } else if (this.classList.contains('pet')) {
-            command = '!pet';
-        } else if (this.classList.contains('mastery')) {
-            command = '!masteries';
-        } else if (this.classList.contains('worldboss')) {
-            command = '!worldboss';
-        } else if (this.classList.contains('env-public')) {
-            command = '!env public';
-        } else if (this.classList.contains('env-test')) {
-            command = '!env test';
-        } else if (this.classList.contains('env-dev')) {
-            command = '!env dev';
-        } else if (this.classList.contains('gamestats')) {
-            command = '!gamestats';
-        } else if (this.classList.contains('gameinfo')) {
-            command = '!gameinfo';
-        }
+document.querySelectorAll(".game-btn").forEach((btn) => {
+  btn.addEventListener("click", function () {
+    let command;
+    if (this.classList.contains("attack")) {
+      command = "!attack";
+    } else if (this.classList.contains("flee")) {
+      command = "!flee";
+    } else if (this.classList.contains("rest")) {
+      command = "!rest";
+    } else if (this.classList.contains("wakeup")) {
+      command = "!wakeup";
+    } else if (this.classList.contains("pet")) {
+      command = "!pet";
+    } else if (this.classList.contains("mastery")) {
+      command = "!masteries";
+    } else if (this.classList.contains("worldboss")) {
+      command = "!worldboss";
+    } else if (this.classList.contains("env-public")) {
+      command = "!env public";
+    } else if (this.classList.contains("env-test")) {
+      command = "!env test";
+    } else if (this.classList.contains("env-dev")) {
+      command = "!env dev";
+    } else if (this.classList.contains("gamestats")) {
+      command = "!gamestats";
+    } else if (this.classList.contains("gameinfo")) {
+      command = "!gameinfo";
+    }
 
-        if (command) {
-            fetch('/send', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ message: command }),
-            });
-        }
+    if (command) {
+      fetch("/send", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ message: command }),
+      });
+    }
+  });
+});
+
+document.querySelectorAll(".dropdown-item").forEach((item) => {
+  item.addEventListener("click", function () {
+    let monsterName = this.querySelector(".monster-title")
+      .textContent.split("|")[1]
+      .trim();
+    let command = `!attack ${monsterName}`;
+
+    fetch("/send", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message: command }),
     });
+  });
 });
 
-document.querySelectorAll('.dropdown-item').forEach(item => {
-    item.addEventListener('click', function() {
-        let monsterName = this.querySelector('.monster-title').textContent.split('|')[1].trim();
-        let command = `!attack ${monsterName}`;
+const ws = new WebSocket("ws://localhost:3000");
 
-        fetch('/send', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ message: command }),
-        });
-    });
+ws.addEventListener("message", function (event) {
+  const data = JSON.parse(event.data);
+  if (data.username.toLowerCase() === "wizebot") {
+    handleWizeBotMessage(data.message);
+  }
 });
 
-const ws = new WebSocket('ws://localhost:3000'); 
+function handleWizeBotMessage(message) {
+  console.log("Message de WizeBot reçu:", message);
+  if (message.includes("📝 (🌍public)")) {
+    updatePlayerProfile(message);
+  }
+}
 
-ws.addEventListener('message', function(event) {
-    const data = JSON.parse(event.data);
-    const chatMessage = `${data.username}: ${data.message}`;
-    const chatMessages = document.getElementById('chatMessages');
-    chatMessages.innerHTML += chatMessage + '<br>';
+document.getElementById("refreshButton").addEventListener("click", function () {
+  fetch("/send", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ message: "!player" }),
+  });
 });
+
+function updatePlayerProfile(message) {
+  if (message.includes("📝 (🌍public)")) {
+    const rankMatch =
+      message.match(/⭐\w+\((.+?)\)/) ||
+      message.match(/(🌍public)\w+\((.+?)\)/);
+    const statsMatch = message.match(/🛡(\d+) 💨(\d+)% 💞(\d+)\(\+(\d+)%\)/);
+    const statsExtraMatch = message.match(/🍀(\d+)%\(\+(\d+)%\) 💰(\d+)/);
+    const combatMatch = message.match(/💥\[(\d+-\d+)\] 👊🏾(\d+)/);
+    const combatExtraMatch = message.match(/👊🏼(\d+) ✨(\d+)%/);
+    const resistMatch = message.match(/🦶(\d+) 💢(\d+)%/);
+    if (rankMatch) {
+      document.querySelector(".player-rank").textContent =
+        "Rang: " + (rankMatch[1] || "???");
+    }
+    if (statsMatch) {
+      document.querySelector(".player-stats").textContent = `🛡 Défense: ${
+        statsMatch[1] || "???"
+      } | 💨 Esquive: ${statsMatch[2] || "???"}% | 💞 PVT: ${
+        statsMatch[3] || "???"
+      } (+${statsMatch[4] || "???"}%)`;
+    }
+    if (statsExtraMatch) {
+      document.querySelector(".player-statss").textContent = `🍀 Chance: ${
+        statsExtraMatch[1] || "???"
+      }% (+${statsExtraMatch[2] || "???"}%) | 💰 Or: ${
+        statsExtraMatch[3] || "???"
+      }`;
+    }
+    if (combatMatch) {
+      document.querySelector(".player-combat").textContent = `💥 Dégâts: ${
+        combatMatch[1] || "???"
+      } | 👊🏾 Nb. coups min.: ${combatMatch[2] || "???"}`;
+    }
+    if (combatExtraMatch) {
+      document.querySelector(
+        ".player-combatt"
+      ).textContent = `👊🏼 Nb. coups max.: ${
+        combatExtraMatch[1] || "???"
+      } | ✨ Crit.: ${combatExtraMatch[2] || "???"}%`;
+    }
+    if (resistMatch) {
+      document.querySelector(".player-resist").textContent = `🦶 Speed: ${
+        resistMatch[1] || "???"
+      } | 💢 Menace: ${resistMatch[2] || "???"}%`;
+    }
+
+    const weaponMatch = message.match(/⚔([^|]+) \|/);
+    if (weaponMatch) {
+      document.querySelector(".arme").textContent =
+        weaponMatch[1].trim() || "???";
+    }
+
+    const equipmentMatch = message.match(/\| ([^|]+) \| [^|]+$/);
+    if (equipmentMatch) {
+      const equipmentItems = equipmentMatch[1].split(", ");
+      document.querySelector(".casque").textContent =
+        equipmentItems[0] || "???";
+      document.querySelector(".plastron").textContent =
+        equipmentItems[1] || "???";
+      document.querySelector(".jambiere").textContent =
+        equipmentItems[2] || "???";
+      document.querySelector(".gant").textContent = equipmentItems[3] || "???";
+      document.querySelector(".pied").textContent = equipmentItems[4] || "???";
+    }
+
+    const accessoryMatch = message.match(/\| (.+?)$/);
+    if (accessoryMatch) {
+      const accessoryItems = accessoryMatch[1].split(", ");
+      document.querySelector(".cape").textContent = accessoryItems[0] || "???";
+      document.querySelector(".ceinture").textContent =
+        accessoryItems[1] || "???";
+    }
+
+    const accessoriesMatches = message.match(
+      /📿=(.+?), 🩹=(.+?), 💍=(.+?), 🔗=(.+?) \|/
+    );
+    if (accessoriesMatches) {
+      document.querySelector(".collier").textContent = `📿 Collier (${
+        accessoriesMatches[1] || "???"
+      })`;
+      document.querySelector(".bracelet").textContent = `🩹 Bracelet (${
+        accessoriesMatches[2] || "???"
+      })`;
+      document.querySelector(".bague").textContent = `💍 Bague (${
+        accessoriesMatches[3] || "???"
+      })`;
+      document.querySelector(".boucle").textContent = `🔗 Boucle d'oreille (${
+        accessoriesMatches[4] || "???"
+      })`;
+    }
+    if (message.startsWith("📝 (🌎test)")) {
+      alert("Veuillez vous connecter au serveur public pour voir votre profil");
+    } else if (message.startsWith("📝 (🌎dev)")) {
+      alert("Veuillez vous connecter au serveur public pour voir votre profil");
+    }
+  }
+}
--- server.js
+++ server.js
@@ -1,53 +1,55 @@
-const tmi = require('tmi.js');
-const express = require('express');
-const bodyParser = require('body-parser');
-const WebSocket = require('ws');
-require('dotenv').config();
-
+const tmi = require("tmi.js");
+const express = require("express");
+const bodyParser = require("body-parser");
+const WebSocket = require("ws");
+require("dotenv").config();
 
 const app = express();
 const port = 3000;
 
 const client = new tmi.Client({
-    options: { debug: true },
-    connection: {
-        reconnect: true,
-        secure: true
-    },
-    identity: {
-        username: process.env.TWITCH_NAME,
-        password: process.env.TWITCH_OAUTH
-    },
-    channels: ['nait0mea']
+  options: { debug: true },
+  connection: {
+    reconnect: true,
+    secure: true,
+  },
+  identity: {
+    username: process.env.TWITCH_NAME,
+    password: process.env.TWITCH_OAUTH,
+  },
+  channels: ["nait0mea"],
 });
 
 const wss = new WebSocket.Server({ noServer: true });
 
 app.use(bodyParser.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 client.connect();
 
-client.on('message', (channel, tags, message, self) => {
-    wss.clients.forEach(client => {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify({ username: tags.username, message: message }));
-        }
-    });
+client.on("message", (channel, tags, message, self) => {
+  if (self) return;
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(
+        JSON.stringify({ username: tags.username, message: message })
+      );
+    }
+  });
 });
 
-app.post('/send', (req, res) => {
-    const { message } = req.body;
-    client.say('nait0mea', message);
-    res.status(200).send('Message envoyé');
+app.post("/send", (req, res) => {
+  const { message } = req.body;
+  client.say("nait0mea", message);
+  res.status(200).send("Message envoyé");
 });
 
 const server = app.listen(port, () => {
-    console.log(`Serveur démarré sur http://localhost:${port}`);
+  console.log(`Serveur démarré sur http://localhost:${port}`);
 });
 
-server.on('upgrade', (request, socket, head) => {
-    wss.handleUpgrade(request, socket, head, socket => {
-        wss.emit('connection', socket, request);
-    });
+server.on("upgrade", (request, socket, head) => {
+  wss.handleUpgrade(request, socket, head, (socket) => {
+    wss.emit("connection", socket, request);
+  });
 });
